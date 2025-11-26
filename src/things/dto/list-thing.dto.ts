9285c24464f284338d0c991d7b66ac5e3a772b24
Ali <alicantokdemir@gmail.com>
2025-11-26 21:09:14 +0100

    chore: cleanup

--- src/things/dto/list-thing.dto.ts
+++
@@ -1,3 +0,0 @@
-import { PaginationDto } from '../../common/pagination';
-
-export class ListThingDto extends PaginationDto {}
--- src/things/things.types.ts
+++
@@ -1,6 +0,0 @@
-import { IBaseRepository } from '../common/base.repository';
-import { Thing } from './entities/thing.entity';
-
-export interface IThingRepository extends IBaseRepository<Thing> {}
-
-export const IThingRepository = Symbol('IThingRepository');
