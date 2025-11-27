8bf914d0c4f072e0b71d82a19719c7e760b332dd
Ali <alicantokdemir@gmail.com>
2025-11-27 16:57:23 +0100

    feat(helpers): helper functions

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -1,5 +1,19 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { hash } from '@node-rs/argon2';
+import { HASHING_OPTIONS } from './constants';
+
 export function isMobile(userAgent: string): boolean {
   const mobileRegex =
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return mobileRegex.test(userAgent);
+}
+
+export async function hashSecret(str: string): Promise<string> {
+  try {
+    return hash(str, HASHING_OPTIONS);
+  } catch (err) {
+    this.logger.error('Failed to hash', err);
+
+    throw new InternalServerErrorException();
+  }
 }
