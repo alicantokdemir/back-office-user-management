0a6cb46b80457f31130bed0cc747e64d19225e1a
Ali <alicantokdemir@gmail.com>
2025-11-27 01:03:02 +0100

    fix: transaction manager pass repositories to set entity manager

--- src/infrastructure/mikroorm/mikroorm-transaction-manager.ts
+++ src/infrastructure/mikroorm/mikroorm-transaction-manager.ts
@@ -1,18 +1,32 @@
 import { EntityManager } from '@mikro-orm/sqlite';
 import { Injectable } from '@nestjs/common';
 import { ITransactionManager } from '../../common/transaction-manager';
+import { BaseMikroormRepository } from './repositories/base.repository';
 
 @Injectable()
 export class MikroOrmTransactionManager implements ITransactionManager {
   constructor(private readonly em: EntityManager) {}
 
-  async saveInTransaction<T>(operation: () => Promise<T>): Promise<T> {
-    return this.em.transactional(async () => {
+  async saveInTransaction<T>(
+    operation: () => Promise<T>,
+    repos: BaseMikroormRepository<any, unknown>[] = [],
+  ): Promise<T> {
+    const em = this.em.fork();
+
+    return em.transactional(async (tem: EntityManager) => {
       try {
+        repos.forEach((repo) => {
+          repo.setEntityManager(tem);
+        });
+
         return await operation();
       } catch (error) {
         console.error('Rolling back transaction: ', error);
         throw error; // rollback is automatic
+      } finally {
+        repos.forEach((repo) => {
+          repo.setEntityManager(this.em);
+        });
       }
     });
   }
